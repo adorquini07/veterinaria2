ef17c4df70424a7d14faafe4ae437eb4cbf9d1f4
Adrian Quinonez <adrianquinonezon@unimagdalena.edu.co>
2024-11-28 14:37:23 -0500

    refactor(USUARIO): :art: UPDATE

---
+++ usuario-microservice/src/main/java/com/unir/usuario_microservice/UsuarioMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.unir.usuario_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsuarioMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsuarioMicroserviceApplication.class, args);
+	}
+
+}
