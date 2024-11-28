8a3a921100a9ec9d2b6a90dede019b7dee199d1f
Adrian Quinonez <adrianquinonezon@unimagdalena.edu.co>
2024-11-28 14:27:52 -0500

    feat(USUARIOS): :art: Creando el microservicio de usuarios

---
+++ usuario-microservice/src/test/java/com/unir/usuario_microservice/UsuarioMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unir.usuario_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuarioMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
