e309de529da767cae90d695363a13eb5525867a5
Adrian Quinonez <adrianquinonezon@unimagdalena.edu.co>
2024-11-29 22:16:11 -0500

    Creando tabla de usuario

---
+++ usuario-microservice/src/main/java/com/unir/usuario_microservice/entity/Usuario.java
@@ -0,0 +1,27 @@
+package com.unir.usuario_microservice.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String password;
+    private String rol;
+    private String estado;
+
+}
